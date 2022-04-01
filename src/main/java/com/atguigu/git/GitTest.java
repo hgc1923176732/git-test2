5d94ea8b9bf5a259a2c9432120d85e4fe3cfd750
guochao.hu <guochao.hu@vtradex.com>
2022-04-01 21:34:44 +0800

    测试iede中git提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description:
+ * @Author guochao.hu
+ * @Date 2022/4/1
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
